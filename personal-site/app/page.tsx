ad6a79c9aa349b05c0d380ae4345246ede568859
Camila Blank <camilab@stanford.edu>
2026-04-03 01:38:36 -0700

    google scholar

--- personal-site/app/page.tsx
+++ personal-site/app/page.tsx
@@ -24,7 +24,7 @@ export default function PersonalSite() {
             <p>
               I am interested in understanding properties of language models and making AI better for society. I currently work on this as a MATS 10.0 scholar with <a href="https://www.neelnanda.io/about" target="_blank" rel="noopener noreferrer" className="hover:underline" style={{color: '#D41795'}}>Neel Nanda</a>. I also make neuroscience foundation models at SAIL, advised by <a href="https://stanford.edu/~eadeli/" target="_blank" rel="noopener noreferrer" className="hover:underline" style={{color: '#D41795'}}>Ehsan Adeli</a>. 
             </p><p>
-              I enjoy playing violin, writing/listening to contemporary classical music, teas, vintage clothing, and many other things.
+              I enjoy playing violin, writing/listening to contemporary classical music, teas, vintage clothing, ethics, and many other things.
                   <section className="mt-6">
                     <h2 className="text-xl mb-2">Recent updates:</h2>
                     <ul className="list-disc list-inside space-y-1">
